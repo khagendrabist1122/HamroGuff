d1b59a2e492b5fb0f19e04e6c2a978766d4e43ae
Khagendra <khagendrabist1122@gmail.com>
2025-12-31 12:24:53 +0545

    Slightly simple modify to the all file.

---
+++ script.js
@@ -0,0 +1,6 @@
+let body = document.querySelector("body");
+let h3 = document.querySelector("h3");
+let p = document.querySelector("p");
+body.style.backgroundColor = "black";
+h3.style.color = "aqua";
+p.style.color = "White";
